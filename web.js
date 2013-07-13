9f1b8bd99887b1b31bfceb6401418438be1fed9a
Ubuntu <ubuntu@ip-172-31-15-3.us-west-2.compute.internal>
2013-07-13 21:58:33 +0000

    Updated web.js for index.html 2

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  var buffer = new Buffer(fs.readFile('index.html'), "utf-8")
+  var string = fs.readFile('index.html');
+  var buffer = new Buffer(string, "utf-8");
   response.send(buffer.toString('utf-8'));
 });
 
